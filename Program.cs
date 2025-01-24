1bd5f1c0f6fab324942087696dcea6847979fc83
dzarenafixers <dzarenafixersinfo@gmail.com>
2025-01-24 13:06:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DZCP.Commands;
+using DZCP.Core;
+using سست.Core;
+
+namespace DZCP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var core = new DzcpCore();
+            core.Start();
+
+            // تسجيل الأوامر
+            CommandManager.RegisterCommand(new ExampleCommand());
+            CommandManager.RegisterCommand(new RoleCommand());
+            CommandManager.RegisterCommand(new RoomCommand());
+            CommandManager.RegisterCommand(new ItemCommand());
+
+            Console.WriteLine("Type 'help' for a list of commands.");
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    core.Stop();
+                    break;
+                }
+                CommandManager.ExecuteCommand(input);
+            }
+        }
+    }
+}
